0e243377a478768a7a7da2295fd7f878b7cbd72b
Серожа <dmitr.demidov2018@yandex.ru>
2020-01-23 21:49:49 +0300

    Описал пошаговые действия!

--- src/ArtificalIntllekt.java
+++ src/ArtificalIntllekt.java
@@ -10,43 +10,63 @@ import java.util.ArrayList;
 public class ArtificalIntllekt {
 
     public static void computerAnotherTurn(ArrayList list) {
+        int turn = 1;
         int j = list.size();
-        if (j > 17 && j <= 20) {
-            int n = list.size() - 17;
+        while (true) {
+            int n = list.size() - (list.size() - 3 * turn);
             for (int m = 0; m < n; m++) {
                 list.remove(list.size() - 1);
             }
             MessegeClass.computrTakeMessage(n);
             MessegeClass.balance(list.size());
-        } else if (j > 13 && j <= 17) {
-            int n = list.size() - 13;
-            for (int m = 0; m < n; m++) {
-                list.remove(list.size() - 1);
+            if (list.size() == 1) {
+                break;
             }
-            MessegeClass.computrTakeMessage(n);
-            MessegeClass.balance(list.size());
-        } else if (j > 9 && j <= 13) {
-            int n = list.size() - 9;
-            for (int m = 0; m < n; m++) {
-                list.remove(list.size() - 1);
-            }
-            MessegeClass.computrTakeMessage(n);
-            MessegeClass.balance(list.size());
-        } else if (j > 5 && j <= 9) {
-            int n = list.size() - 5;
-            for (int m = 0; m < n; m++) {
-                list.remove(list.size() - 1);
-            }
-            MessegeClass.computrTakeMessage(n);
-            MessegeClass.balance(list.size());
-        } else if (j > 1 && j <= 5) {
-            int n = list.size() - 1;
-            for (int m = 0; m < n; m++) {
-                list.remove(list.size() - 1);
-            }
-            MessegeClass.computrTakeMessage(n);
-            MessegeClass.balance(list.size());
-            MessegeClass.computerWin();
         }
+        MessegeClass.computerWin();
     }
 }
+
+
+
+
+
+//        if (j > 17 && j <= 20) {
+//            int n = list.size() - 17;
+//            for (int m = 0; m < n; m++) {
+//                list.remove(list.size() - 1);
+//            }
+//            MessegeClass.computrTakeMessage(n);
+//            MessegeClass.balance(list.size());
+//        } else if (j > 13 && j <= 17) {
+//            int n = list.size() - 13;
+//            for (int m = 0; m < n; m++) {
+//                list.remove(list.size() - 1);
+//            }
+//            MessegeClass.computrTakeMessage(n);
+//            MessegeClass.balance(list.size());
+//        } else if (j > 9 && j <= 13) {
+//            int n = list.size() - 9;
+//            for (int m = 0; m < n; m++) {
+//                list.remove(list.size() - 1);
+//            }
+//            MessegeClass.computrTakeMessage(n);
+//            MessegeClass.balance(list.size());
+//        } else if (j > 5 && j <= 9) {
+//            int n = list.size() - 5;
+//            for (int m = 0; m < n; m++) {
+//                list.remove(list.size() - 1);
+//            }
+//            MessegeClass.computrTakeMessage(n);
+//            MessegeClass.balance(list.size());
+//        } else if (j > 1 && j <= 5) {
+//            int n = list.size() - 1;
+//            for (int m = 0; m < n; m++) {
+//                list.remove(list.size() - 1);
+//            }
+//            MessegeClass.computrTakeMessage(n);
+//            MessegeClass.balance(list.size());
+
+//        }
+//    }
+//}
